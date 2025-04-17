f34f7074732da0b6fdfeb459daaa97501d8a5205
kim-yebeen <kimyebeen125@gmail.com>
2025-04-18 02:40:09 +0900

    로그인/회원가입 token 전송

--- src/main/java/com/nine/baseballdiary/backend/auth/JwtProvider.java
+++ src/main/java/com/nine/baseballdiary/backend/auth/JwtProvider.java
@@ -2,9 +2,12 @@ package com.nine.baseballdiary.backend.auth;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -19,25 +22,37 @@ public class JwtProvider {
     @Value("${jwt.refresh-expiration}")
     private long refreshExpirationInMs;
 
+    /**
+     * AccessToken 생성
+     */
     public String createAccessToken(String subject) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + accessExpirationInMs);
+
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.builder()
                 .setSubject(subject)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 
+    /**
+     * RefreshToken 생성
+     */
     public String createRefreshToken(String subject) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + refreshExpirationInMs);
+
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.builder()
                 .setSubject(subject)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 }
