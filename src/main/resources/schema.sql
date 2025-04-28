436a5376eb574067654aa33dcf51861f5f6c07e7
kim-yebeen <kimyebeen125@gmail.com>
2025-04-28 13:45:03 +0900

    record 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     kakao_id BIGINT UNIQUE NOT NULL,
-                                     nickname VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    kakao_id BIGINT UNIQUE NOT NULL,
+    nickname VARCHAR(50) NOT NULL,
     profile_image_url TEXT,
     body VARCHAR(100),
     fav_team VARCHAR(50),
@@ -10,11 +10,11 @@ CREATE TABLE IF NOT EXISTS users (
     );
 
 CREATE TABLE IF NOT EXISTS game (
-                                    game_id VARCHAR PRIMARY KEY,
-                                    date DATE NOT NULL,
-                                    time TIME,
-                                    playtime TIME,
-                                    stadium VARCHAR(50),
+    game_id VARCHAR PRIMARY KEY,
+    date DATE NOT NULL,
+    time TIME,
+    playtime TIME,
+    stadium VARCHAR(50),
     home_team VARCHAR(50),
     away_team VARCHAR(50),
     home_score INT,
@@ -25,9 +25,11 @@ CREATE TABLE IF NOT EXISTS game (
     );
 
 CREATE TABLE IF NOT EXISTS record (
-                                      record_id SERIAL PRIMARY KEY,
-                                      user_id INT REFERENCES users(id),
+    record_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
     game_id VARCHAR REFERENCES game(game_id),
+    ticket_image_url TEXT,
+    seat_info VARCHAR(100),
     comment TEXT,
     emotion_emoji VARCHAR(20),
     best_player VARCHAR(50),
@@ -39,12 +41,12 @@ CREATE TABLE IF NOT EXISTS record (
     );
 
 CREATE TABLE IF NOT EXISTS stadium (
-                                       id SERIAL PRIMARY KEY,
-                                       name VARCHAR(50) NOT NULL
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS food (
-                                    id SERIAL PRIMARY KEY,
-                                    fnb_name VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    fnb_name VARCHAR(50) NOT NULL,
     stadium_id INT REFERENCES stadium(id)
     );
