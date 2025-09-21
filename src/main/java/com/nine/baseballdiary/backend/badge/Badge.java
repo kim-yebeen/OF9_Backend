0ed545755651745a55ed6a2917c3d2e1b9e9f795
kim-yebeen <kimyebeen125@gmail.com>
2025-09-22 01:25:47 +0900

    [add] report API

--- src/main/java/com/nine/baseballdiary/backend/badge/Badge.java
+++ src/main/java/com/nine/baseballdiary/backend/badge/Badge.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 public class Badge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private long id;
 
     @Column(nullable = false)
     private String category;
