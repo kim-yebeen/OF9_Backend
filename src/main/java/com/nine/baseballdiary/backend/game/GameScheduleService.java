3f7301ddae7925508fd45013b68bf84abeb5f593
kim-yebeen <kimyebeen125@gmail.com>
2025-09-22 21:39:00 +0900

    [fix]크롤링업데이트

--- src/main/java/com/nine/baseballdiary/backend/game/GameScheduleService.java
+++ src/main/java/com/nine/baseballdiary/backend/game/GameScheduleService.java
@@ -36,7 +36,7 @@ public class GameScheduleService {
     }
 
     // GitHub Actions 배포 후 크롤링 실행 (UTC 11:48)
-    @Scheduled(cron = "0 40 21 * * *")
+    @Scheduled(cron = "0 50 21 * * *")
     public void dailyFullCrawl() {
         logger.info("전체 크롤링 시작 - " + LocalDate.now());
         crawlSchedule(true);
