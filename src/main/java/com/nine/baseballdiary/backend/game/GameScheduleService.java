1dabb41b5184ae958d092eb35e19305ff9742d21
kim-yebeen <kimyebeen125@gmail.com>
2025-10-04 09:46:48 +0900

    [add] REPORT API 구현

--- src/main/java/com/nine/baseballdiary/backend/game/GameScheduleService.java
+++ src/main/java/com/nine/baseballdiary/backend/game/GameScheduleService.java
@@ -37,7 +37,7 @@ public class GameScheduleService {
     }
 
 
-    @Scheduled(cron = "0 38 9 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 52 9 * * *", zone = "Asia/Seoul")
     public void dailyUpdate11() {
         logger.info("일일 업데이트 시작 (오전 11시) - " + LocalDate.now());
         crawlSchedule(true);
