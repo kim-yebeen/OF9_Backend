17e64ae3473098a2d3eed22ff5e7749786342fb4
kim-yebeen <kimyebeen125@gmail.com>
2025-10-05 11:21:18 +0900

    [fix] s3 수정

--- src/main/java/com/nine/baseballdiary/backend/BackendApplication.java
+++ src/main/java/com/nine/baseballdiary/backend/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-@EnableScheduling
+
 @SpringBootApplication
 public class BackendApplication {
 
---
+++ src/main/java/com/nine/baseballdiary/backend/common/config/SchedulingConfiguration.java
@@ -0,0 +1,27 @@
+package com.nine.baseballdiary.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration implements SchedulingConfigurer {
+
+    private static final int POOL_SIZE = 10;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("kbo-schedule-"); // 로그에서 확인하기 쉽도록 스레드 이름 설정
+        threadPoolTaskScheduler.initialize();
+
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
+
+
