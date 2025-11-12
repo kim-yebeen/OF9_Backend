2daf2c36c4d2918481805c0001fd81bb1a7752ce
kim-yebeen <kimyebeen125@gmail.com>
2025-11-13 01:44:58 +0900

    [fix] 3 badges -> 5 badges

--- src/main/java/com/nine/baseballdiary/backend/record/GameRecord.java
+++ src/main/java/com/nine/baseballdiary/backend/record/GameRecord.java
@@ -33,17 +33,17 @@ public class GameRecord {
     private String longContent;
     private String bestPlayer;
 
-    @ElementCollection
+    @ElementCollection(fetch=FetchType.EAGER)
     @CollectionTable(name = "record_companions", joinColumns = @JoinColumn(name = "record_id"))
     @Column(name = "companion_id")
     private List<Long> companions;
 
-    @ElementCollection
+    @ElementCollection(fetch=FetchType.EAGER)
     @CollectionTable(name = "record_food_tags", joinColumns = @JoinColumn(name = "record_id"))
     @Column(name = "tag")
     private List<String> foodTags;
 
-    @ElementCollection
+    @ElementCollection(fetch=FetchType.EAGER)
     @CollectionTable(name = "record_media_urls", joinColumns = @JoinColumn(name = "record_id"))
     @Column(name = "url", columnDefinition = "TEXT")
     private List<String> mediaUrls;
